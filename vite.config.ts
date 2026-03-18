59233d0e361ed7f42bbc13c8a929777ccce84069
Jesús Enrique Peñaloza Piñero <enrique.penaloza.working@gmail.com>
2026-03-18 12:52:33 -0400

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import { defineConfig } from 'vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
@@ -6,4 +7,10 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '&': path.resolve(__dirname, './.chadcn'),
+    },
+  },
 });

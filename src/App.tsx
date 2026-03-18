86612436448a2f6e5841d1267c6492f25ef7cfa7
Jesús Enrique Peñaloza Piñero <enrique.penaloza.working@gmail.com>
2026-03-18 14:59:20 -0400

    instala y configura chadcn

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from './assets/vite.svg';
 import heroImg from './assets/hero.png';
-import './App.css';
+import { Button } from '#/components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -21,9 +21,9 @@ function App() {
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
-        <button className="counter" onClick={() => setCount((count) => count + 1)}>
+        <Button className="counter" onClick={() => setCount((count) => count + 1)}>
           Count is {count}
-        </button>
+        </Button>
       </section>
 
       <div className="ticks"></div>

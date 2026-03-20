8c98419881c5cebf360fd01649e31f4fedf18206
Jesús Enrique Peñaloza Piñero <enrique.penaloza.working@gmail.com>
2026-03-19 22:23:05 -0400

    Instala y configura vitest

---
+++ __test__/main.test.tsx
@@ -0,0 +1,19 @@
+import { describe, test, expect, beforeEach, afterEach } from 'vitest';
+
+describe('main', () => {
+  beforeEach(() => {
+    // Setup code here
+  });
+
+  afterEach(() => {
+    // Teardown code here
+  });
+
+  test('Should ', () => {
+    expect(true).toBe(true);
+  });
+
+  test('Should ', () => {
+    expect(true).toBe(true);
+  });
+});

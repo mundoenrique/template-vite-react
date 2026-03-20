3e45e52a32efc0fb88cfc2397d7654295626b361
Jesús Enrique Peñaloza Piñero <enrique.penaloza.working@gmail.com>
2026-03-20 07:52:17 -0400

    refactor: configura vite.conig

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import path from 'node:path';
 import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
